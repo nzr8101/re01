9fddc1f142c85e09fa03ad51881bbd5753c55632
nzr <2601188384@qq.com>
2024-11-16 15:57:01 +0800

    newbranch

--- helloworld.c
+++ helloworld.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    printf("helloworld");
+
+    printf("hellonong\n");
+    printf("hellonongshen\n");
+    printf("helloworld\n");
+    printf("helloworld\n");
+    printf("helloworld\n");
     return 0;
-
-
+    
 
 }
