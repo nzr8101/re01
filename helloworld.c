2ff5de0b746b873ca4d0640048d2455c3d49b94d
nzr <2601188384@qq.com>
2024-11-16 16:02:04 +0800

    b1

--- helloworld.c
+++ helloworld.c
@@ -2,11 +2,8 @@
 int main()
 {
 
-    printf("hellonong\n");
+
     printf("hellonongshen\n");
-    printf("helloworld\n");
-    printf("helloworld\n");
-    printf("helloworld\n");
     return 0;
     
 
