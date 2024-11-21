786dc072d5b129bc0891ce882d1efcc8722dd4ff
nzr8101 <2601188384@qq.com>
2024-11-21 16:25:54 +0800

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -7,7 +7,10 @@ int main()
     printf("man bo\n");
     printf("man bo\n");
     
-
+    printf("ou ye ！\n");
+    printf("ou ye ！\n");
+    printf("ou ye ！\n");
+    printf("ou ye ！\n");
     return 0;
     
 
